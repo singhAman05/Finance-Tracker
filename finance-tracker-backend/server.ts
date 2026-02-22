a06d84a32a6c90ad8bfaab9d75012be0e6f4dd38
singhAman05 <amanshankarsingh05@gmail.com>
2026-02-22 21:09:14 +0530

    Adding new changes

--- finance-tracker-backend/server.ts
+++ finance-tracker-backend/server.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import http from 'http'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import loginRoute from "./routes/route_auth"
+import billRoute from "./routes/route_bills";
+import budgetRoute from "./routes/route_budgets";
 import profileRoute from "./routes/route_profile"
 import accountsRoute from "./routes/route_accounts"
 import categoryRoute from "./routes/route_categories"
 import transactionRoute from "./routes/route_transactions";
-import budgetRoute from "./routes/route_budgets";
+
+dotenv.config();
 
 const app = express()
 const server = http.createServer(app)
@@ -23,6 +27,9 @@ app.use(`/api/accounts`, accountsRoute);
 app.use(`/api/category`, categoryRoute);
 app.use(`/api/transactions`, transactionRoute)
 app.use(`/api/budgets`, budgetRoute)
-server.listen(8000, () => {
-    console.log("The server is listening at 8000");
+app.use(`/api/bills`, billRoute)
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`The server is listening at ${PORT}`);
 })
